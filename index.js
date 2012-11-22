52fe9e897cb893dec734c20bd82ed84b760014c4
Rafael Garcia <rgarcia2009@gmail.com>
2012-11-21 16:33:47 -0800

    updating version, also forgot an export

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ if (process.env.COV_GEARMAN) {
   module.exports = {
     Gearman: require('./lib-js-cov/gearman'),
     Client: require('./lib-js-cov/client'),
-    Worker: require('./lib-js-cov/worker')
+    Worker: require('./lib-js-cov/worker'),
+    Protocol: require('./lib/protocol')
   };
 } else {
   module.exports = {
     Gearman: require('./lib/gearman'),
     Client: require('./lib/client'),
-    Worker: require('./lib/worker')
+    Worker: require('./lib/worker'),
+    Protocol: require('./lib/protocol')
   };
 }
